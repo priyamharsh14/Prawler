b9ec836d8564880b6272b4f72319aa7cd38dc389
Priyam Harsh <42106642+priyamharsh14@users.noreply.github.com>
2019-08-08 07:55:49 +0530

    Uploads

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+	name = 'Prawler',
+	packages = ['Prawler'],
+	version = '0.1',
+	description = 'Scrape Proxies from Internet in seconds',
+	author = 'Priyam Harsh',
+	author_email = 'priyamharsh14@gmail.com',
+	url = 'https://github.com/priyamharsh14/Prawler',
+	download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+	keywords = ['proxy', 'scraper', 'crawler', 'proxycrawler'],
+	install_requires=['requests'],
+	data_files=[('', ['LICENSE'])],
+	include_package_data=True,
+	classifiers=[
+	'Development Status :: 4 - Beta',
+	'Intended Audience :: Developers',
+	'Topic :: Software Development :: Libraries :: Python Modules',
+	'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+	'Programming Language :: Python :: 3',
+	'Programming Language :: Python :: 3.4',
+	'Programming Language :: Python :: 3.5',
+	'Programming Language :: Python :: 3.6',
+	'Operating System :: OS Independent',
+	'Environment :: Console',
+	],
+)
