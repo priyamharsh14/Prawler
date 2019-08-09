08332f2cb66081682db911863db6e3dedf19ec05
Priyam Harsh <42106642+priyamharsh14@users.noreply.github.com>
2019-08-09 23:31:53 +0530

    Updates

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
 	'Programming Language :: Python :: 3.4',
 	'Programming Language :: Python :: 3.5',
 	'Programming Language :: Python :: 3.6',
+	'Programming Language :: Python :: 3.7',
 	'Operating System :: OS Independent',
 	'Environment :: Console',
 	],
